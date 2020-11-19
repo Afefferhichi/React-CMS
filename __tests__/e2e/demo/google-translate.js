0d3050f4adc0dfe8001d4608489a8529f6792bf7
Afefferhichi <afef.ferchichi95@gmail.com>
2020-11-19 23:36:45 +0800

    update

--- __tests__/e2e/demo/google-translate.js
+++ __tests__/e2e/demo/google-translate.js
@@ -3,7 +3,7 @@ import {Selector} from 'testcafe';
 const credentials = require('../config/credentials.json');
 
 fixture`Login`
-  .page`https://translate.google.com/?sl=ko&tl=zh-CN&text=%EC%9D%B4%20%EC%9A%94%EB%A6%AC%EB%8A%94%20%20%EB%84%88%EB%AC%B4%20%EC%A7%9C%EC%9A%94.&op=translate/`
+  .page`/`
 
 let testCount = 0;
 test(`${++testCount}. Should login with valid credentials`, async t => {
